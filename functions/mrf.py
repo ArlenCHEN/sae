5f3cb7e16d943d4c2a223d1f68d99f1396dbfe0c
Ubuntu <ubuntu@ip-172-31-0-38.us-east-2.compute.internal>
2021-02-07 19:18:50 +0000

    results

--- functions/mrf.py
+++ functions/mrf.py
@@ -1,5 +1,5 @@
 import torch
-import skimage 
+import skimage
 import numpy as np
 from torch import nn
 import torch.nn.functional as F
@@ -11,17 +11,18 @@ def get_lookup(prior, neighboor_size):
             shape should be [1, labels, dim1, dim2, dim3]
     '''
     k = neighboor_size
+
     assert prior.dtype == torch.uint8, 'The prior should be one-hot encoded byte'
-    assert np.any(np.linspace(3,21,10) == k), ('Make sure that the neighboour_size' + 
+    assert np.any(np.linspace(3,21,10) == k), ('Make sure that the neighboour_size' +
                                                'is within np.linspace(3,21,10)')
     labels = prior.size(1)
-    
+
     enumerate_chs = torch.arange(labels).view(1, -1, 1, 1, 1).byte()
     enumerated_prior = enumerate_chs*prior
-    enumerated_prior = torch.sum(enumerated_prior,1,True) 
+    enumerated_prior = torch.sum(enumerated_prior,1,True)
     enumerated_prior = tt.padder(enumerated_prior.float(), k)
     enumerated_prior = enumerated_prior.squeeze().int().numpy()
-    windows = skimage.util.view_as_windows(enumerated_prior, (k,k,k)) # windows.shape 
+    windows = skimage.util.view_as_windows(enumerated_prior, (k,k,k)) # windows.shape
                                                                       #(dim1, dim2, dim3, k, k, k)
     centers = windows[:, :, :, k//2, k//2, k//2]
 
@@ -30,12 +31,12 @@ def get_lookup(prior, neighboor_size):
 
     lookup_table = np.zeros((labels, labels))
     for condition in range(labels):
-        idx = (centers == condition)   
+        idx = (centers == condition)
         for s in range(labels):
             if condition == s:
                 # removing repeated counts that comes from the center
                 counts = np.sum(windows[idx] == s) - windows[idx].shape[0]
-            else: 
+            else:
                 counts = np.sum(windows[idx] == s)
             lookup_table[condition, s] = counts
 
@@ -44,24 +45,24 @@ def get_lookup(prior, neighboor_size):
                   keepdims=True)
     norm = np.tile(norm, (1, labels))
 
-    lookup_table = lookup_table/norm 
+    lookup_table = lookup_table/norm
 
-    assert (np.allclose(lookup_table.sum(1), 
+    assert (np.allclose(lookup_table.sum(1),
                         np.ones_like(lookup_table.sum(1)))), 'Row doesnt add up to 1'
-    
+
     return lookup_table
 
 def neighboor_q(input, neighboor_size):
     '''
     Calculate the product of all q(s|x) around voxel i
     Uses convolution to sum the log(q_y) then takes the exp
-    
+
     input: prob of q
     '''
-    
+
     k = neighboor_size
-    assert np.any(np.linspace(3,21,10) == k), ('Make sure that the neighboour_size' + 
-                                               'is within np.linspace(3,21,10)')    
+    assert np.any(np.linspace(3,21,10) == k), ('Make sure that the neighboour_size' +
+                                               'is within np.linspace(3,21,10)')
     x = tt.padder(input,
                   kernel_size= k)
 
@@ -72,37 +73,37 @@ def neighboor_q(input, neighboor_size):
     filter = filter.repeat(chs,1,1,1,1).float().cuda()
     filter.requires_grad = False
 
-    out = F.conv3d(x, 
+    out = F.conv3d(x,
                    weight= filter,
-                   stride= 1, 
+                   stride= 1,
                    groups= chs)
     return out
 
 def spatial_consistency(input, table, neighboor_size):
     '''
     KL divergence between q(s|x) and markov random field
-    
+
     input: prob of q
     table: lookup table as probability. Rows add up to 1
     '''
     eps = 1e-12
     n_batch, chs, dim1, dim2, dim3 = input.shape
-    q_i = input 
+    q_i = input
     q_y = neighboor_q(input, neighboor_size)
     assert q_i.shape == q_y.shape, 'q_y and q_i should be the same shape'
-    
+
     # To log probability table
-    assert (np.allclose(table.sum(1), 
+    assert (np.allclose(table.sum(1),
                         np.ones_like(table.sum(1)))), 'Row doesnt add up to 1'
-    
+
     M = torch.from_numpy(table+eps).float()
     M = M/torch.sum(M, 1, True) #Normalize to account for the extra eps
     M = torch.log(M).cuda()
     assert M.shape == torch.Size([chs,chs]), 'Table dims dont match number of labels'
     M = M.view(1, chs, chs)
-    
+
     #Multiplication
-    q_i = input.view(n_batch, chs, dim1*dim2*dim3) 
+    q_i = input.view(n_batch, chs, dim1*dim2*dim3)
     q_y = q_y.view(n_batch, chs, dim1*dim2*dim3)
     out = torch.bmm(M, q_y)  # shape [n_batch, chs, dim1*dim2*dim3]
     out = torch.sum(q_i*out,1)
--- functions/visualization.py
+++ functions/visualization.py
@@ -5,18 +5,18 @@ import matplotlib.pyplot as plt
 import torch.nn.functional as F
 from skimage.filters import gaussian
 
-def overlay_regions(mri, 
+def overlay_regions(mri,
                     regs,
-                    image_idx, 
-                    brain_oppacity=0.6, 
-                    show_brain=True, 
+                    image_idx,
+                    brain_oppacity=0.6,
+                    show_brain=True,
                     ax=None,
-                    title='Overlay', 
+                    title='Overlay',
                     s=None):
     '''
     mri: [n_batch, 1, x_dim, y_dim, z_dim] torch tensor
     regs: [n_batch, n_labels, x_dim, y_dim, z_dim] torch tensor
-    
+
     '''
     mri = mri.detach().cpu()
     regs = regs.detach().cpu()
@@ -25,16 +25,16 @@ def overlay_regions(mri,
         mri = torch.from_numpy(mri).float()
     if regs.dtype is not torch.float32:
         regs = torch.from_numpy(regs).float()
-    
-    regs = torch.clamp(regs,0,1) 
-    mri = torch.clamp(mri,0,1)   
+
+    regs = torch.clamp(regs,0,1)
+    mri = torch.clamp(mri,0,1)
     n_batch, n_labels, x_dim, y_dim, z_dim = regs.shape
-    
+
     if s is None:
         _s = z_dim//2
     else:
         _s = s
-    
+
     mri_slice = mri[image_idx, 0, :, :, _s]
     mri_slice = np.swapaxes(mri_slice.float().numpy(),0,1)
 
@@ -43,20 +43,20 @@ def overlay_regions(mri,
     #Last color is white, which might be confusing
     cmap = matplotlib.cm.get_cmap('tab20c')
     palette = ((1.0,1.0,1.0),) + cmap.colors
-  
+
     if ax is None:
         fig, ax = plt.subplots()
-    else: 
+    else:
         ax = ax
-    for i in range(n_labels):   
+    for i in range(n_labels):
         if i == 0:
             continue
         slice = regs[image_idx, i, :, :, _s].float().numpy()
         slice = np.swapaxes(slice,0,1)
         alpha = np.expand_dims(slice,2)
-        rbg = np.asarray(palette[i]).reshape(1,1,-3)    
+        rbg = np.asarray(palette[i]).reshape(1,1,-3)
         slice_rgb = rbg*np.tile(np.expand_dims(slice, 2),(1,1,3))
-        slice_rgba = np.concatenate([slice_rgb, alpha], 2)  
+        slice_rgba = np.concatenate([slice_rgb, alpha], 2)
 
         ax.imshow(slice_rgba)
 
@@ -72,15 +72,15 @@ def overlay_regions(mri,
         mri_rbga = np.concatenate((mri_slice, alpha), 2)
 
         ax.imshow(mri_rbga)
-          
+
     ax.axis('off')
     ax.set_title(title)
 
-    
+
 def argmax_ch(input):
     '''
     Pick the most likely class for each pixel
-    individual mask: each subjects 
+    individual mask: each subjects
     have different uniformly sample mask
     '''
     input = input.detach().cpu()
@@ -91,10 +91,10 @@ def argmax_ch(input):
 
     arange = torch.arange(0,chs).view(1,-1, 1, 1, 1)
     arange = arange.repeat(batch_n, 1, 1, 1, 1).float()
-    
+
     enumerate_ch = torch.ones(batch_n, chs, xdim, ydim, zdim)
- 
-    enumerate_ch = arange*enumerate_ch 
+
+    enumerate_ch = arange*enumerate_ch
 
     classes = torch.argmax(input,1).float()
     sample = []
@@ -102,64 +102,63 @@ def argmax_ch(input):
         _sample = enumerate_ch[:,c,:,:,:] == classes
         sample += [_sample.unsqueeze(1)]
     sample = torch.cat(sample, 1)
-    
     return sample
 
-def view_results(target_brain, 
-                 recon_brain, 
+def view_results(target_brain,
+                 recon_brain,
                  pred_regs,
                  target_regs,
-                 epoch, 
-                 suffix, 
+                 epoch,
+                 suffix,
                  show_brain,
                  image_idx=0,
-                 PATH=None, 
-                 show_image=False, 
+                 PATH=None,
+                 show_image=False,
                  s =None):
     if s is None:
         _s = target_brain.shape[4]//2
     else:
         _s = s
-        
+
     # Cross-section
     _target_brain = target_brain
     recon_brain = recon_brain[image_idx,0,:,:,_s].data.cpu().numpy()
     target_brain = target_brain[image_idx,0,:,:,_s].data.cpu().numpy()
 
     fig, ax = plt.subplots(nrows=1, ncols=4)
-    
+
     ax[0].set_title('Recon')
     recon_brain = np.clip(recon_brain, 0, 1)
     image = np.swapaxes(recon_brain,0,1)
     ax[0].imshow(image, cmap='gray', vmin=0, vmax=1)
-    ax[0].axis('off')    
-      
+    ax[0].axis('off')
+
     ax[1].set_title('Target')
     image = np.swapaxes(target_brain,0,1)
     ax[1].imshow(image, cmap='gray', vmin=0, vmax=1)
     ax[1].axis('off')
-    
-    overlay_regions(_target_brain, 
+
+    overlay_regions(_target_brain,
                     pred_regs,
                     image_idx,
                     brain_oppacity=0.2,
                     show_brain=show_brain,
-                    ax=ax[2], 
-                    title='Predicted', 
+                    ax=ax[2],
+                    title='Predicted',
                     s= _s)
-    
-    overlay_regions(_target_brain, 
+
+    overlay_regions(_target_brain,
                     target_regs,
                     image_idx,
                     brain_oppacity=0.2,
                     show_brain=show_brain,
-                    ax=ax[3], 
-                    title='Target', 
-                    s= _s)    
+                    ax=ax[3],
+                    title='Target',
+                    s= _s)
     if PATH is not None:
         fig.set_size_inches(15, 5)
         fig.savefig(PATH+str(epoch)+suffix)
     elif show_image:
         plt.show()
-        
+
     plt.close('all')
